25516801888e0dad0447f4d394df0052786380ac
Spoonieau <spoonie.woonie@gmail.com>
2021-03-20 12:08:35 +0800

    Added PS3 Controller Support

--- GamePad-Control/main.py
+++ GamePad-Control/main.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
 #coding: utf-8
 
+# Written by spoonie (Rick Spooner) for RobotsGo 
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import os
 import sys
 import RPi.GPIO as GPIO
@@ -14,6 +29,7 @@ import servo
 import argparse
 import xboxOneMapping
 import steamMapping
+import ps3Mapping
 
 def exitApp():
     print("Clear LED's")
@@ -40,7 +56,7 @@ GAME_PAD_SELECTED = int(0)
 # Initialize parser
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--camera", help="Disable mpeg streamer", action='store_true')
-parser.add_argument("-g", "--gamepad", help="GamePad you would like to use to control the robot. Options are XboxOne or Steam", type=str)
+parser.add_argument("-g", "--gamepad", help="GamePad you would like to use to control the robot. Options are XboxOne Steam or PS3", type=str)
 parser.add_argument("-v", "--version", help="See version and build information", action='store_true')
     
 # Read arguments from command line
@@ -60,9 +76,13 @@ if(args.gamepad):
     elif(args.gamepad == 'Steam'):
         GAME_PAD_SELECTED = int(2)
         print("GamePad in use will be: Steam Controller")
+        
+    elif(args.gamepad == 'PS3'):
+        GAME_PAD_SELECTED = int(3)
+        print("GamePad in use will be: PS3 Controller")
     
     else:
-        print("Incorrect controller entered, neither XboxOne or Steam")
+        print("Incorrect controller entered, neither XboxOne, Steam or PS3 ")
         sys.exit()
 
 if(args.version):
@@ -78,6 +98,9 @@ if(GAME_PAD_SELECTED == 1):
     
 if(GAME_PAD_SELECTED == 2):
     steamMapping.steamMappingStart()
+    
+if(GAME_PAD_SELECTED == 3):
+    ps3Mapping.ps3MappingStart()
     
 exitApp()
 
