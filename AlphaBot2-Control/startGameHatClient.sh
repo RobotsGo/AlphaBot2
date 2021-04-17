5ebd693a136c046936442682d775b898cbe7a82c
Spoonieau <spoonie.woonie@gmail.com>
2021-04-17 12:24:03 +0800

    fixes

--- AlphaBot2-Control/startGameHatClient.sh
+++ AlphaBot2-Control/startGameHatClient.sh
@@ -1,28 +1,15 @@
-#!/bin/bash
-
-#!/bin/sh
-
-start_Client(){
-    echo "Start GameHat Client"
-    echo python3 gameHatMapping.py -s $server &
-    disown
-}
-
-start_VLC(){
-    echo "Stat VLC and connect to video stream"
-    vlc -f http://$server:8000/video_feed
-}
-
-launcher_exit() {
-    pkill vlc
-    exit
-}
-
-
 server=`cat ip.txt`
 echo "AlphBot2 streaming and WaveShare GameHat control client version 0.1"
 echo "Will try and connect to AlphaBot @: $server"
 
-start_Client
-start_VLC
-launcher_exit
+echo "Stat VLC and connect to video stream" 
+cvlc -f 'http://'$server':8000/video_feed' & 
+
+echo "Start GameHat Client"
+python3 gameHatMapping.py -s $server
+
+pkill vlc
+
+echo "exit"
+
+
