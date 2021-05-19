65c95fd6c4450339f53b9b9b0bdb13094bf214cf
Spoonieau <spoonie.woonie@gmail.com>
2021-05-19 20:27:55 +0800

    Start ustreamer

--- AlphaBot2-Control/startStreamer.sh
+++ AlphaBot2-Control/startStreamer.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-export FLASK_APP=streamer.py
 
 host=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
 echo $host
 
-flask run -h $host -p 8000 --with-threads
+ustreamer --format=YUYV --brightness=55 --encoder=omx --workers=3 --persistent --drop-same-frames=30 --host=$host --port=8000 
